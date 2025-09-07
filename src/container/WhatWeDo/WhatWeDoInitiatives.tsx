a21edff21918671234b54298bda5901ffb4558bf
Noel Uba <ubanoel2000@gmail.com>
2025-09-07 21:51:28 +0100

    Fix build

--- src/container/WhatWeDo/WhatWeDoInitiatives.tsx
+++ src/container/WhatWeDo/WhatWeDoInitiatives.tsx
@@ -69,7 +69,7 @@ const WhatWeDoInitiatives = () => {
                 </div>
                 <div className="flex flex-col font-inter font-normal justify-center leading-[0] min-w-full not-italic relative shrink-0 text-[#546165] text-[16px] lg:text-[18px]">
                   <p className="leading-[24px] lg:leading-[26px]">
-                    Mental health matters. JCI's continental campaign is raising
+                    Mental health matters. JCI&apos;s continental campaign is raising
                     awareness of mental health issues and encouraging people to
                     seek help when they need it.
                   </p>
